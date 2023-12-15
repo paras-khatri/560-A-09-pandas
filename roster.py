d33fccb6e52483bd56bb7bcbf7052b417a314404
paras-khatri <paraskhatri99@gmail.com>
2023-12-15 16:23:39 -0500

    pandas as pd

--- roster.py
+++ roster.py
@@ -1,6 +1,7 @@
 # https://goheels.com/sports/mens-basketball/roster 
+import pandas as pd
 
 roster = {"High", "Ryan", "Davis"}
-for player in roster: 
-    print(player)
-    
+data = pd.DataFrame(roster)
+print(data)
+
