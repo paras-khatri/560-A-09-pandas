28a7c5b29c57634b4d5651b58c46a78d0a39a568
paras-khatri <paraskhatri99@gmail.com>
2023-12-15 17:37:32 -0500

    10 Players Final Commit

--- roster.py
+++ roster.py
@@ -2,15 +2,15 @@
 
 import pandas as pd
 
-player = {"Last Name": ["High", "Ryan", "Davis"],
-          "First Name": ["Zayden", "Cormac", "RJ"],
-          "height": [81, 77, 72],
-          "weight": [225, 195, 180]}
+player = {"Last Name": ["High", "Ryan", "Davis", "Cadeau", "Bacot", "Trimble", "Wojcik", "Washington", "Lebo", "Landry"],
+          "First Name": ["Zayden", "Cormac", "RJ", "Elliot", "Armando", "Seth", "Paxson", "Jalen", "Creighton", "Rob"],
+          "height": [81, 77, 72, 73, 83, 75, 77, 82, 73, 76],
+          "weight": [225, 195, 180, 180, 240, 195, 195, 230, 180, 190]}
 
 data = pd.DataFrame(player)
 
 # bmi = weight in kg/height in meters squared 
-data["bmi"] = (data["weight"]/2.205)/((data["height"]/39.37)**2)
+data["bmi"] = round((data["weight"]/2.205)/((data["height"]/39.37)**2),2)
 
 print(data)
 
