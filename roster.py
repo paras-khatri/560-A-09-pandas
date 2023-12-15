acae72d6865a6214e3350ed697763f3cb3087c48
paras-khatri <paraskhatri99@gmail.com>
2023-12-15 16:48:38 -0500

    BMI Update

--- roster.py
+++ roster.py
@@ -6,6 +6,13 @@ player = {"Last Name": ["High", "Ryan", "Davis"],
           "First Name": ["Zayden", "Cormac", "RJ"],
           "height": [81, 77, 72],
           "weight": [225, 195, 180]}
+
 data = pd.DataFrame(player)
+
+# bmi = weight in kg/height in meters squared 
+data["bmi"] = (data["weight"]/2.205)/((data["height"]/39.37)**2)
+
 print(data)
+
+data.to_csv("bmi.csv")
 
